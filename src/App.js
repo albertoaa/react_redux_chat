7819ffc4c0ce20c06cdcef0e972139f9b05f1195
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-18 13:23:34 -0500

    Optimizing imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Sidebar } from "./containers/Sidebar";
-import { MessagesList } from "./containers/MessagesList";
 import { AddMessage } from "./containers/AddMessage";
+import { MessagesList } from "./containers/MessagesList";
+import { Sidebar } from "./containers/Sidebar";
 
 class App extends Component {
   render() {
