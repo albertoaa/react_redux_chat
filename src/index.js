d75738197f67675970f3b46c7330106bff775c92
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-18 12:10:23 -0500

    Rendering user on Sidebar UserList

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { addUser } from './actions';
 
 import chat from './reducers';
 
 const store = createStore(chat);
 
+store.dispatch(addUser('Me'));
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />, document.getElementById('root')
-  </Provider>
+    <App />
+  </Provider>, document.getElementById('root')
 );
 registerServiceWorker();
