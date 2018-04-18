62aa8cc93123220170df57b99ab9775b788e6642
Alberto Aragon <alberto@alturasoluciones.com>
2018-04-18 11:20:56 -0500

    Refactoring layout into components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Sidebar } from "./containers/Sidebar";
+import { MessagesList } from "./containers/MessagesList";
+import { AddMessage } from "./containers/AddMessage";
 
 class App extends Component {
   render() {
     return (
       <div id="container">
-        <aside id="sidebar">Users</aside>
+        <Sidebar />
         <section id="main">
-          <section id="message-list">Messages List</section>
-          <section id="new-message">New Message</section>
+          <MessagesList />
+          <AddMessage />
         </section>
       </div>
     );
